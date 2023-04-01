6b8172ef7713f5d53846bb2ec2f1cec6f682b0d7
Tyapor718 <katepsupport@mail.com>
2023-04-01 13:08:17 +0400

    Таблица квадратов

---
+++ example04/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = ReadInt ("Введите число N");
+for (int i= 1; i <= number; i++)
+{
+    System.Console.Write($"{Math.Pow(i,2)}  >");
+}
