0a4ca380071de6ef875b252a75df15949f6126f2
Tyapor718 <katepsupport@mail.com>
2023-04-01 12:59:07 +0400

    Определение длины строки

---
+++ example03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int ax = ReadInt ("Координата х первой точки");
+int ay = ReadInt ("Координата y первой точки");
+int bx = ReadInt ("Координата х второй точки");
+int by = ReadInt ("Координата y второй точки");
+
+int koordX = (ax - bx);
+int koordY = (ay - by);
+double W = Math.Sqrt(Math.Pow(koordX,2) + Math.Pow(koordY,2));
+System.Console.Write($"Показать результат {W:f2}");
+//$ знак подстановки который позволяет внутри строки писать переменные в {}
+
